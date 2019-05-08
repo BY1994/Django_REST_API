be3df9f0436c025260305ea9aa2c73401256f5b5
Boyoun Park <lovelychoco100@gmail.com>
2019-05-08 07:03:24 +0000

    settings.py 에 필요한 내용들 추가

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hy1ws1o5yk*ycard0gs^+d&x5q7hrwww0*53zy=_i45tfepy8)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes'
 ]
 
 MIDDLEWARE = [
