a6a7fd63012c8eaec74c1c39274253d5027c0786
Boyoun Park <lovelychoco100@gmail.com>
2019-05-08 07:54:41 +0000

    api 서버의 GET 방식과 POST 방식의 처리 방식을 정의

---
+++ notes/serializers.py
@@ -0,0 +1,8 @@
+# serializers 쿼리셋을 제이슨 타입의 형태로 포팅해주는 친구이다.
+from rest_framework import serializers
+from .models import Memo
+
+class MemoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Memo
+        fields = ['content', ]
--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Memo
+from .serializers import MemoSerializer
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def create_and_list(request):
+    if request.method == 'POST':
+        serializer = MemoSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+    else:
+        memos = Memo.objects.all()
+        serializer = MemoSerializer(memos, many=True)
+    return Response(serializer.data)
